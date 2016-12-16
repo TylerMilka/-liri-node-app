0a45a1e675047b5efbc998ecc6d2f4a70e5fa869
Tyler <tgmilka@gmail.com>
2016-12-16 16:29:00 -0600

    NADA

---
+++ key.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<Sod7Y9V4uCD9W3Gx3SNhtkToP>',
+  consumer_secret: '<PoTrBIT9XNQlJvMP9dCCFAmd6Tb1sFxUd0b1hX84Z3iGUjMwwm>',
+  access_token_key: '<2520101128-dIU2BsHwmhJrXgsOI2gjPUz5V9mo9EAYdgGg8B6>',
+  access_token_secret: '<LuSNgudMZ6yWyE2f5N5QQ9uCB7zpKFTssSiAkuowaW3v4>',
+}
+
---
+++ liri.js
@@ -0,0 +1,8 @@
+var apiKeys = require("./keys.js");
+
+var apiKeys.twitterkeys;
+
+window.ready(function(){
+console.log(apiKeys)
+
+});
